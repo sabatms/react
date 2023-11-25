0ece3459dbb6edd9c01ea14b3019157f6d13eacb
Saba Tahmasebi <saba.tms15@gmail.com>
2023-11-25 02:08:08 -0800

    add src file episode-2

--- episode-1/src/App.js
+++ episode-1/src/App.js
@@ -1,36 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import"./App.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <User name="saba" age={24} email="salam@saba123" />
+//       <User name="ali" age={43} email="none" />
+//       <User name="shaqayeq" age={66} email="--" />
+
+//       <Jobs salary={100000000} position="developer" company="Google" />
+//       <Jobs salary={200000000} position="front-end developer" company="Amazon" />
+//       <Jobs salary={300000000} position="back-end developer" company="Snapp" />
+//     </div>
+//   );
+// }
+
+// const User = (props) =>{
+//   return(
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.age}</h2>
+//       <h3>{props.email}</h3>
+//     </div>
+//   )
+// }
+
+// const Jobs =(props) =>{
+//   return(
+//     <div>
+//       {props.salary} ,{props.position},{props.company}
+//     </div>
+//   )
+// }
 
 function App() {
-  return (
-    <div>
-      <User name="saba" age={24} email="salam@saba123" />
-      <User name="ali" age={43} email="none" />
-      <User name="shaqayeq" age={66} email="--" />
 
-      <Jobs salary={100000000} position="developer" company="Google" />
-      <Jobs salary={200000000} position="front-end developer" company="Amazon" />
-      <Jobs salary={300000000} position="back-end developer" company="Snapp" />
+    // <div style={{ color: "red" , fontSize : "30px" }}>
+    //     Sabatahmasebi
+    // </div>
+      
+    const age = 22;
+   return(
+    <div className="App">
+      {age > 20 ? <h1>over</h1> : <h1>under</h1>}
+      
+      {age === 20 && <h1>age is 20</h1>}
     </div>
-  );
-}
-
-const User = (props) =>{
-  return(
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h3>{props.email}</h3>
-    </div>
-  )
-}
-
-const Jobs =(props) =>{
-  return(
-    <div>
-      {props.salary} ,{props.position},{props.company}
-    </div>
-  )
+   )
+ 
 }
 
 export default App;
